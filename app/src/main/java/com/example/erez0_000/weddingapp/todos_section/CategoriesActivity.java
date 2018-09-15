99a8c123a96496dd2cb94ebe3353c81581bb7850
erez <erez@cs.huji.ac.il>
2018-09-15 13:12:54 +0300

    fixed firebaseRecycler.
    added category activity

--- app/src/main/java/com/example/erez0_000/weddingapp/todos_section/CategoriesActivity.java
+++ app/src/main/java/com/example/erez0_000/weddingapp/todos_section/CategoriesActivity.java
@@ -35,7 +35,7 @@ public class CategoriesActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_categories);
         initRecyclerView();
         listOfTodos = new ArrayList<>();
         taskText = findViewById(R.id.ed_main);
