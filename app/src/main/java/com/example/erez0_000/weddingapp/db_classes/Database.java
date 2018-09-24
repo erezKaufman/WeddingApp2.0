925df87190f53b6776f421b524b988bd81f189ba
Ben Faingold <benfaingold@gmail.com>
2018-09-24 20:43:08 +0300

    Start working on signin/signup with mongo

--- app/src/main/java/com/example/erez0_000/weddingapp/db_classes/Database.java
+++ app/src/main/java/com/example/erez0_000/weddingapp/db_classes/Database.java
@@ -1,6 +1,8 @@
 package com.example.erez0_000.weddingapp.db_classes;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
@@ -8,7 +10,7 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public class Database {
-    public static final String BASE_URL = "https://api.mlab.com/api/1/databases/wedding_app/collections";
+    private static final String BASE_URL = "https://api.mlab.com/api/1/databases/wedding_app/collections";
     private static final String API_KEY = "22IUrrMtRgv9WsRc8Nm9Ov72z9orvB1c";
 
     private static Database db;
@@ -24,12 +26,20 @@ public class Database {
         service = new Retrofit.Builder().baseUrl(BASE_URL).build().create(MlabService.class);
     }
 
+    public void signin(String username, String password, Callback<User> callback) {
+        service.getUser(username, password).enqueue(callback);
+    }
+
+    public void signup(String username, String password, Callback<User> callback) {
+        service.addUser(username, password).enqueue(callback);
+    }
+
     public interface MlabService {
         @GET("Users?q={\"username\": {u}, \"password\": {p}}&apiKey=" + API_KEY)
         Call<User> getUser(@Path("u") String username, @Path("p") String password);
 
         @POST("Users?apiKey=" + API_KEY)
-        Call addUser(@Field("username") String username, @Field("password") String password);
+        Call<User> addUser(@Field("username") String username, @Field("password") String password);
 
         // filter
     }
--- app/src/main/java/com/example/erez0_000/weddingapp/db_classes/User.java
+++ app/src/main/java/com/example/erez0_000/weddingapp/db_classes/User.java
@@ -3,15 +3,16 @@ package com.example.erez0_000.weddingapp.db_classes;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public String email, username, age, number, type, cost, area, season;
+    public String email, username, password, age, number, type, cost, area, season;
 
     public User() {}
 
-    public User(String email,String username,String age,String number,String type,String cost,
+    public User(String email,String username, String password, String age,String number,String type,String cost,
                 String area,String season){
         this.age = age;
         this.email = email;
         this.username = username;
+        this.password = password;
         this.number = number;
         this.type = type;
         this.cost = cost;
